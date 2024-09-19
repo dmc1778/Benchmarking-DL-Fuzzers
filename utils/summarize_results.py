09954214fd6e454a161d58cf5554b0fac7464c8e
dmc1778 <nimashiri2012@gmail.com>
2024-09-19 04:05:34 -0400

    deeprel test count added

--- utils/summarize_results.py
+++ utils/summarize_results.py
@@ -10,6 +10,16 @@ def write_to_csv(data, toolname):
         write = csv.writer(file_writer)
         write.writerow(data)
 
+def list_python_files(directory):
+    python_files = []
+    
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.py'):
+                python_files.append(os.path.join(root, file))
+
+    return python_files
+
 class SummarizeTestCases:
     def __init__(self, tool_name, lib_name, iteration, release) -> None:
         self.tool_name = tool_name
@@ -35,20 +45,30 @@ class SummarizeTestCases:
         
         self.titanfuzz_root_path = f"/media/nimashiri/DATA/testing_results/tosem/titanfuzz/Results/{self.lib_name}/{self.release}/{self.iteration}"
         self.atlasfuzz_root_path = f"/media/nimashiri/DATA/testing_results/tosem/code-{self.tool_name}/fewshot/output/{self.lib_name}_demo/{self.iteration}/{self.release}"
-    
-    def count_freefuzz_test_cases(self):
-        if self.lib_name == 'torch':
-            target_data = read_txt('data/torch_apis.txt')
-        else:
-            target_data = read_txt('data/tf_apis.txt')
 
-        test_counter = {
+        self.freefuzz_test_counter = {
             'fail': 0,
             'potential-bug': 0,
             'success': 0,
             'crash': 0,
             'timeout': 0
         }
+
+        self.deeprel_test_counter = {
+            'bug': 0,
+            'err': 0,
+            'fail': 0,
+            'neq': 0,
+            'success': 0
+        }
+        
+    def count_freefuzz_test_cases(self):
+        if self.lib_name == 'torch':
+            target_data = read_txt('data/torch_apis.txt')
+        else:
+            target_data = read_txt('data/tf_apis.txt')
+
+
         directory_path = os.listdir(self.freefuzz_root_path)
         directories = [item for item in directory_path if os.path.isdir(os.path.join(self.freefuzz_root_path, item))]
         for dir_ in directories:
@@ -61,17 +81,43 @@ class SummarizeTestCases:
                         test_files_path = os.path.join(current_oracle, api)
                         test_files_list = os.listdir(test_files_path)
                         py_file_count = len([file for file in test_files_list if file.endswith('.py')])
-                        test_counter[oracle] += py_file_count
+                        self.freefuzz_test_counter[oracle] += py_file_count
                         
         crashed_tests = read_txt(os.path.join(self.freefuzz_root_path,'runcrash.txt'))
         timedout_tests = read_txt(os.path.join(self.freefuzz_root_path,'timeout.txt'))
         
-        test_counter['crash'] = len(crashed_tests)
-        test_counter['timeout'] = len(timedout_tests)
+        self.freefuzz_test_counter['crash'] = len(crashed_tests)
+        self.freefuzz_test_counter['timeout'] = len(timedout_tests)
         
-        output_data = [self.lib_name, self.iteration, self.release] + list(test_counter.values())
+        output_data = [self.lib_name, self.iteration, self.release] + list(self.freefuzz_test_counter.values())
         write_to_csv(output_data, self.tool_name)
-                        
+        self.freefuzz_test_counter = {key: 0 for key in self.freefuzz_test_counter}
+        
+    def count_deeprel_test_cases(self):
+        if self.lib_name == 'torch':
+            target_data = read_txt('data/torch_apis.txt')
+        else:
+            target_data = read_txt('data/tf_apis.txt')
+
+        directories = ['output-0']
+        for dir_ in directories:
+            current_dir = os.path.join(self.deeprel_root_path, dir_)
+            all_dirs = os.listdir(current_dir)
+            for pair in all_dirs:
+                if "+" not in pair:
+                    continue
+                current_api_pair= os.path.join(current_dir, pair)
+                current_oracles = os.listdir(current_api_pair)
+                for oracle in ['bug', 'err', 'fail', 'neq', 'success']:
+                    target_ = os.path.join(current_api_pair, oracle)
+                    if not os.path.exists(target_):
+                        continue
+                    test_files_list = os.listdir(target_)
+                    py_file_count = len([file for file in test_files_list if file.endswith('.py')])
+                    self.deeprel_test_counter[oracle] += py_file_count
+        output_data = [self.lib_name, self.iteration, self.release] + list(self.deeprel_test_counter.values())
+        write_to_csv(output_data, self.tool_name)
+        self.deeprel_test_counter = {key: 0 for key in self.deeprel_test_counter}
                         
 if __name__ == '__main__':
     
@@ -79,10 +125,9 @@ if __name__ == '__main__':
         'torch': ['2.0.0', '2.0.1', '2.1.0'],
         'tf': ['2.11.0', '2.12.0', '2.13.0', '2.14.0'],
     }
-    iterations = [1,2,3,4,5]
     for k, v in lib.items():
-        for iteration in iterations:
+        for iteration in range(1, 6):
             for release in v:
                 print(f'Library: {k}, Iteration: {iteration}, Release: {release}')
-                obj_= SummarizeTestCases('FreeFuzz', k, iteration, release)
-                obj_.count_freefuzz_test_cases()
+                obj_= SummarizeTestCases('DeepRel', k, iteration, release)
+                obj_.count_deeprel_test_cases()
