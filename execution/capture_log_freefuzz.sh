b2c2119f89b88c3df57f4cc4691ce652b630be8c
dmc1778 <nimashiri2012@gmail.com>
2024-09-23 19:47:16 -0400

    FreeFuzz log capture updated. It has a bug while trying to iterate over target test files.

--- execution/capture_log_freefuzz.sh
+++ execution/capture_log_freefuzz.sh
@@ -8,6 +8,7 @@ dir_=$5
 oracle=$6
 api=$7
 iteration=$8
+fullAddr=$9
 
 source /home/nimashiri/anaconda3/etc/profile.d/conda.sh
 conda activate "$env_name"
@@ -15,14 +16,17 @@ conda activate "$env_name"
 CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib
 
-ROOT_DIR="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version/$dir_/$oracle/$api"
+output_file="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version/$source_version.txt"
+{ echo "Processing file: $fullAddr"; timeout 60s python "$fullAddr"; } |& tee -a "$output_file"
+
+# ROOT_DIR="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version/$dir_/$oracle/$api"
 #log_directory="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version"
 
 # mkdir -p "$log_directory"
 
 #touch "$log_directory$source_version.txt"
 
-output_file="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version/$source_version.txt"
+# output_file="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$source_version/$source_version.txt"
 
 
 # find "$ROOT_DIR" -type f -name "*.py" | while read file
@@ -31,13 +35,12 @@ output_file="/media/nimashiri/DATA/testing_results/tosem/$tool/$lib/$iteration/$
 #     { echo "Processing file: $file"; timeout 60s python "$file"; } |& tee -a "$output_file"
 # done
 
-files=$(find "$ROOT_DIR" -type f -name "*.py")
+# files=$(find "$ROOT_DIR" -type f -name "*.py")
 
-for file in $files; do
-    echo "Processing file: $file"
-    timeout 60s python "$file" |& tee -a "$output_file"
-    # Check if the process was interrupted
-    if [ $? -eq 124 ]; then
-        echo "Timeout reached for $file"
-    fi
-done
+# for file in $files; do
+#     echo "Processing file: $file" timeout 60s python "$file" |& tee -a "$output_file"
+#     # Check if the process was interrupted
+#     if [ $? -eq 124 ]; then
+#         echo "Timeout reached for $file"
+#     fi
+# done
--- execution/postprocess_freefuzz_tests.py
+++ execution/postprocess_freefuzz_tests.py
@@ -1,7 +1,15 @@
 import re, csv, sys, subprocess, os, glob, shutil
+import pandas as pd
+sys.path.insert(0, '/home/nimashiri/Benchmarking-DL-Fuzzers/')
+# from execution.decompose_log import decompose_detections
+
 REG_PTR = re.compile('Processing file')
 REG_PTR_ORION = re.compile('Running')
 
+def read_txt(_path):
+    with open(_path, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
 
 def decompose_detections(splitted_lines):
     super_temp = []
@@ -38,11 +46,6 @@ def decompose_detections(splitted_lines):
 
     return super_temp
 
-def read_txt(_path):
-    with open(_path, "r") as f:
-        lines = [line.strip() for line in f.readlines()]
-    return lines
-
 def capture_output(lib, iteration,_version, env_name, tool) -> None:
     _path_to_logs_old = f"/media/nimashiri/DATA/testing_results/tosem/{tool}/{lib}/{iteration}/{_version}"
     if lib == 'torch':
@@ -58,49 +61,63 @@ def capture_output(lib, iteration,_version, env_name, tool) -> None:
         for oracle in ['potential-bug']:
             current_oracle = os.path.join(current_dir, oracle)
             current_apis = os.listdir(current_oracle)
-            for api in current_apis:
-                if api in target_data:
-                    shell_command = ["post_processing/capture_log_freefuzz.sh",lib, _version, tool, env_name, dir_, oracle, api, str(iteration)]
-                    subprocess.call(shell_command,shell=False)
+            for api_dir in current_apis:
+                api_under_test = os.path.join(current_oracle, api_dir)
+                list_apis = os.listdir(api_under_test)
+                for file in list_apis:
+                    file_to_run = os.path.join(api_under_test, file)
+                    if api_dir in target_data:
+                        shell_command = ["execution/capture_log_freefuzz.sh",lib, _version, tool, env_name, dir_, oracle, api_dir, str(iteration), file_to_run]
+                        subprocess.call(shell_command,shell=False)
 
-def save_freefuzz_logs(lib, tool, release):
+def detect_bugs(lib, iteration,_version, tool):
     if lib == 'torch':
         target_data = read_txt('data/torch_apis.txt')
+        ground_truth = pd.read_csv(f'data/{lib}_groundtruth.csv')
     else:
         target_data = read_txt('data/tf_apis.txt')
+        ground_truth = pd.read_csv(f'data/{lib}_groundtruth.csv')
     
-    log_data_old = f"/media/nimashiri/DATA/testing_results/tosem/{tool}/{lib}/{release}/{release}.txt"
-    output_dir = f"/media/nimashiri/DATA/testing_results/tosem/{tool}/{lib}/{release}/"
-    log_data_latest = read_txt(log_data_old)
+    _path_to_logs_old = f"/media/nimashiri/DATA/testing_results/tosem/{tool}/{lib}/{iteration}/{_version}/{_version}.txt"
+    output_path  = f"/media/nimashiri/DATA/testing_results/tosem/{tool}"
+    log_data_latest = read_txt(_path_to_logs_old)
     log_decomposed = decompose_detections(log_data_latest)
-    for log in log_decomposed:
-        if "Processing file" in log[0]:
-            test_err_full = log[-5:]
-            test_err_symp = log[-1]
-            api_name = log[0].split('/')[-2]
-            
-            if api_name in target_data:
-                if '.py' in api_name:
-                    api_name = api_name.replace('.py', '')
+    for idx, row in ground_truth.iterrows():
+        for log in log_decomposed:
+            if "Processing file" in log[0]:
+                api_name = log[0].split('/')[-3]
                 print(api_name)
-
-                decom_ = ''.join(log)
-                output = [tool, release, api_name, decom_]
-                
-                with open(f"{output_dir}{release}.csv", "a", encoding="utf-8", newline='\n') as file:
-                    write = csv.writer(file)
-                    write.writerow(output)
-            else:
-                print('Not in target data!')
+                if row['Buggy API'] == api_name:
+                    if '.py' in api_name:
+                        api_name = api_name.replace('.py', '')
+                    log_merged = ''.join(log)
+                    pattern = re.compile(row['Log Rule'])
+                    match = pattern.search(log_merged)
+                    if match and row['Version'] == _version:
+                        output = [tool, lib, iteration, row['Version'], _version, api_name, row['Log Message'], log_merged]
+                        
+                        with open(f"{output_path}/detected_bugs.csv", "a", encoding="utf-8", newline='\n') as file:
+                            write = csv.writer(file)
+                            write.writerow(output)
+                    else:
+                        print('No detection')
 
 
 def main():
-    lib = sys.argv[1]
-    iteration = sys.argv[2]
-    release = sys.argv[3]
-    env_name = sys.argv[4]
-
-    capture_output(lib, iteration, release, env_name, 'FreeFuzz')
+    # lib = sys.argv[1]
+    # iteration = sys.argv[2]
+    # release = sys.argv[3]
+    # env_name = sys.argv[4]
+    # capture_output(lib, iteration,release, env_name, 'FreeFuzz')
+    capture_output('torch', 1, '2.0.0', 'torch_2.0.0', 'FreeFuzz')
+    # for lib in ['torch', 'tf']:
+    #     for i in range(1, 5):
+    #         if lib == 'tf':
+    #             releases = ["2.11.0", "2.12.0", "2.13.0", "2.14.0"]
+    #         else:
+    #             releases = ['2.0.0', '2.0.1', '2.1.0']
+    #         for release in releases:
+    #             detect_bugs(lib, i, release, 'FreeFuzz')
 
 if __name__ == '__main__':
     main()
